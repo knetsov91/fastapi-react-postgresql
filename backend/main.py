a4f7109048e24af486fb1b53308088a0f27937dc
Konstantin <knetsov91@gmail.com>
2024-08-15 16:25:09 +0300

    Added /ner endpoint that returns text's named entites as JSON.

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Body
+from typing import Annotated
+from ml.ner import retrieve_named_entites
 app = FastAPI()
 
 
 @app.get("/")
 def index():
     return "index"
+
+@app.post("/ner")
+async def get_entities(text: Annotated[str,  Body()]  = "das"):
+    print(text)
+    # return ""
+    return retrieve_named_entites(text)  
