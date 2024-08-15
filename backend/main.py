32ec78ada4ab59d29e859f1b7b2e8fb4a4a3b818
Konstantin <knetsov91@gmail.com>
2024-08-15 20:21:35 +0300

    Add CORS support for React frontend.

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,24 @@
 from fastapi import FastAPI, Body
 from typing import Annotated
+from fastapi.middleware.cors import CORSMiddleware
 from ml.ner import retrieve_named_entites
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="http://localhost:3000",
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/")
 def index():
     return "index"
 
+
 @app.post("/ner")
-async def get_entities(text: Annotated[str,  Body()]  = "das"):
+async def get_entities(text: Annotated[str,  Body(embed=True)]  = "das"):
     print(text)
     # return ""
     return retrieve_named_entites(text)  
