f7326319608a425718dec184fac81e58131c7b87
Konstantin <knetsov91@gmail.com>
2024-08-16 00:51:07 +0300

    Added  test for retrieve_named_entites() that check when input is one word, correct answer is returned.

--- backend/tests/test_ner.py
+++ backend/tests/test_ner.py
@@ -5,3 +5,7 @@ class TestClass:
     def test_ner_whenTextIsOneSymbol_thenRaiseException(self):
         with pytest.raises(Exception):    
             retrieve_named_entites("d")
+
+    def test_ner_whenTextIsOneWord_thenReturnListOfCorrectObjects(self):
+        res = retrieve_named_entites("Paris")
+        assert res == [{ "entity": "Paris",  "label": "GPE" }]
