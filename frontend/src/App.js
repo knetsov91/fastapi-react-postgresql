293a5cf75cb37bc63ea8f41253a5932af9a3a03a
Konstantin <knetsov91@gmail.com>
2024-08-15 20:27:16 +0300

    Add React component for work with NER.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
-
+import NerComponent from './components/NerComponent';
 function App() {
   return (
     <div className="App">
         <p>Index</p>
+        <NerComponent />
     </div>
   );
 }
