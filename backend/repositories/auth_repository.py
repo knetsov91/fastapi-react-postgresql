59048335df0c3b412fa07099f27e3333d0f5a069
Konstantin <knetsov91@gmail.com>
2024-08-29 10:40:02 +0300

    Add register() in auth_repository.

---
+++ backend/repositories/auth_repository.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Session
+from data.schemas.User import UserLogin
+from data.models.UserModel import User
+
+
+def register(db_conn: Session, user: UserLogin):
+
+    new_user = User(email = user.email, password = user.password)
+
+    db_conn.add(new_user)
+    db_conn.commit()
+    db_conn.refresh(new_user)
+
+    return new_user
