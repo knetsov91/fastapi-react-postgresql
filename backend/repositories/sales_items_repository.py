0deede6a809d30599e0639f58cf9b6d010195da9
Konstantin <knetsov91@gmail.com>
2024-08-29 15:06:05 +0300

    Add create_sales_item() in sales_items_repository.py

---
+++ backend/repositories/sales_items_repository.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Session
+from data.schemas.SalesItem import SalesItemCreate
+from data.models.SalesItem import SalesItemModel
+
+
+def create_sales_item(db: Session, sales_item: SalesItemCreate):
+    new_sales_item = SalesItemModel(**sales_item.dict())
+
+    db.add(new_sales_item)
+    db.commit()
+    db.refresh(new_sales_item)
+    
+    return new_sales_item
