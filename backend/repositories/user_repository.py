0edbdcc9112627489305b4c96f12f895a5a89f47
Konstantin <knetsov91@gmail.com>
2024-08-29 15:06:35 +0300

    Add get_user_by_id() in user_repository.py

--- backend/repositories/user_repository.py
+++ backend/repositories/user_repository.py
@@ -2,9 +2,12 @@ from sqlalchemy.orm import Session
 from data.schemas.User import UserLogin
 from data.models.UserModel import User
 
-def get_user_by_email( db: Session, email:str):
+def get_user_by_email( db: Session, email:str) -> User:
    
    return db.query(User).filter(User.email == email).first()
+
+def get_user_by_id(db: Session, id: int):
+   return db.query(User).filter(User.id == id).first()
 
 def get_users(db: Session ):
     return db.query(User).all()
