3a683debe9e829a2dd0123de4222a39d43cb2525
Konstantin <knetsov91@gmail.com>
2024-08-29 10:27:44 +0300

    Add SQLAlchemy User model.

---
+++ backend/data/models/UserModel.py
@@ -0,0 +1,11 @@
+from utils.database.database import Base
+from sqlalchemy.orm import  relationship
+from sqlalchemy import Integer, String, Column, Boolean, DateTime
+from datetime import datetime
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
+    
