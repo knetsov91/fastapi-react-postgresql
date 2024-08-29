baef0b06bac714cdf30b623686d7901d4608af81
Konstantin <knetsov91@gmail.com>
2024-08-29 10:48:42 +0300

    Add FastAPI main file with /login endpoint and configured connection to Postgres.

--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,45 @@
 from fastapi import FastAPI, Body
 from typing import Annotated
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
 from ml.ner import retrieve_named_entites
+import os
+from utils.database.database import SessionLocal, engine, Base
+from dotenv import load_dotenv
+from sqlalchemy.orm import Session
+from repositories import user_repository, auth_repository
+from utils.database.database import SessionLocal, engine
+from data.schemas.User import UserLogin,User
+from  data.schemas.User import User as UserSchema  
+from  data.models import UserModel
+from fastapi import Depends
+
+load_dotenv()
 app = FastAPI()
 
+origins = [f"localhost:{os.getenv('FRONTEND_PORT')}",
+           f"http://localhost:{os.getenv('FRONTEND_PORT')}"]
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+Base.metadata.create_all(bind=engine)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="http://localhost:3000",
+    allow_origins=origins,
     allow_methods=["*"],
     allow_headers=["*"]
 )
 
-@app.get("/")
-def index():
-    return "index"
 
 
-@app.post("/ner")
-async def get_entities(text: Annotated[str,  Body(embed=True)]  = "das"):
-    print(text)
-    # return ""
-    return retrieve_named_entites(text)  
+@app.post("/login", response_model=UserSchema )
+def login(user: UserLogin , db: Session= Depends(get_db)):
+    print(user)
+    user_login = auth_repository.register(db,user )
+    return user_login
+
+
