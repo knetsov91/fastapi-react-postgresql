0a9f9143a383a64cbd744052218824706ec4d53a
Konstantin <knetsov91@gmail.com>
2024-08-29 15:01:04 +0300

    Add SalesItem SQLAlchemy model

---
+++ backend/data/models/SalesItem.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, ForeignKey, String, Integer
+from utils.database.database import Base
+
+class SalesItemModel(Base):
+    __tablename__ = "items"
+    id = Column(Integer, primary_key=True,autoincrement=True)
+    description = Column(String, nullable=False)
+    price = Column(Integer)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="items")
