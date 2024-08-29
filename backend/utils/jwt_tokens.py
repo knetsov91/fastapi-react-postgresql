91a50597608e01f71b2626d425eacf97f66971ee
Konstantin <knetsov91@gmail.com>
2024-08-29 21:22:05 +0300

    Add generate_jwt().

--- backend/utils/jwt_tokens.py
+++ backend/utils/jwt_tokens.py
@@ -1,5 +1,8 @@
 import jwt
 
+def generate_jwt(payload, secret,alg= "HS256"):
+    return jwt.encode(payload=payload, key=secret, algorithm= alg)
+
 def validate_jwt_token(jwt_token, secret, alg="HS256"):
     try:
         return jwt.decode(jwt_token, key=secret,algorithms=alg)
