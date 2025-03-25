740a681ffc504037ed7826e0780724dce7e552cf
Konstantin <knetsov91@gmail.com>
2025-03-25 22:34:09 +0200

    Add /home route.

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,30 +1,34 @@
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import Register from './components/Auth/Register';
-import Layout from './components/Auth/Layout';
-import Home from './components/Auth/Home';
+import Layout from './components/Pages/Layout';
+import Home from './components/Pages/Home';
 import Login from './components/Auth/Login';
+import ProtectedRoute from './components/ProtectedRoute';
 const Routes = ({children}) => {
 
-
-    const router = createBrowserRouter([
-        
-        {
-            path: "/",
-            element: <Layout />,
-            children: [
-                {index: true, element: <Home />},
-                {
-                    path: "/register",
-                    element: <Register />
-                },
-                {
-                    path: "/login",
-                    element: <Login />
-                }
-            ]
+    const router = createBrowserRouter([    
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {index: true, element: <Home />},
+            {
+                path: "/register",
+                element: <Register />
+            },
+            {
+                path: "/login",
+                element: <Login />
+            },
             
-        },
-        
+    ]},
+    {
+        path: "/home",
+        element:<Layout />,
+        children: [
+            {index: true, element: <ProtectedRoute><Home /></ProtectedRoute>}
+        ]
+    } 
     ])
 
     return (
