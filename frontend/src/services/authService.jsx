a865d99b83ce54777fd004fc376b0119947bc54c
Konstantin <knetsov91@gmail.com>
2025-03-25 20:41:38 +0200

    Add login service method for sending POST request to the backend.

--- frontend/src/services/authService.jsx
+++ frontend/src/services/authService.jsx
@@ -1,11 +1,17 @@
 import axios from 'axios';
+const BACKEND_URL = "http://localhost:8000"; 
 
 export const register = (email, password) => {
-    axios.post("http://localhost:8000/register", {email, password})
+    axios.post(`${BACKEND_URL}/register`, {email, password})
     .then(data => {
         console.log(data);
         
     })
     .catch(err => console.log(err))
 };
+
+export const login = (email, password) => {
+
+    return axios.post(`${BACKEND_URL}/login`, {email, password});
+} 
 
