f47ffde6125eb0355e66fe4ab4f591a74ff5d263
Konstantin <knetsov91@gmail.com>
2025-03-25 20:36:03 +0200

    Move components to directory.

---
+++ frontend/src/components/Pages/Navbar.jsx
@@ -0,0 +1,21 @@
+import {Link} from 'react-router';
+import Register from '../Auth/Register';
+
+const Navbar = () => {
+
+
+    return (
+        <>
+        <ul>
+          <li>
+            <Link to="/register"> Register</Link>
+            <Link to="/login"> Login</Link>
+          </li>  
+        </ul>
+    
+        </>
+    );
+        
+}
+
+export default Navbar;
