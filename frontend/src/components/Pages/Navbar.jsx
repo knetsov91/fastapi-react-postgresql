ee1bc54ade3709df8390b58bd4324b088f8138e5
Konstantin <knetsov91@gmail.com>
2025-03-26 08:15:06 +0200

    Show email in navbar if user is authenticated.

--- frontend/src/components/Pages/Navbar.jsx
+++ frontend/src/components/Pages/Navbar.jsx
@@ -1,9 +1,17 @@
 import {Link} from 'react-router';
 import Register from '../Auth/Register';
+import { useAuth } from '../../context/authContext';
 
 const Navbar = () => {
 
+    const {authToken} = useAuth()
+    let email;
+    if (authToken) {
+      email = JSON.parse(atob(authToken.split(".")[1])).email
 
+    }
+    console.log(email);
+    
     return (
         <>
         <ul>
@@ -11,9 +19,10 @@ const Navbar = () => {
             <Link to="/register"> Register</Link>
             <Link to="/login"> Login</Link>
             <Link to="/users"> Users</Link>
+            <Link to="/logout"> Logout</Link>
           </li>  
         </ul>
-    
+        {email && <p>Hello {email }<b></b></p> }
         </>
     );
         
