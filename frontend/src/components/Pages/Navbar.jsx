71360ba0757bf6204f62b73195ea09836feff48c
Konstantin <knetsov91@gmail.com>
2025-03-26 08:32:36 +0200

    Move JWT decoding in separate function.

--- frontend/src/components/Pages/Navbar.jsx
+++ frontend/src/components/Pages/Navbar.jsx
@@ -1,25 +1,31 @@
 import {Link} from 'react-router';
 import Register from '../Auth/Register';
 import { useAuth } from '../../context/authContext';
-
+import {decodeJwt} from '../../utils';
 const Navbar = () => {
 
     const {authToken} = useAuth()
+    
     let email;
     if (authToken) {
-      email = JSON.parse(atob(authToken.split(".")[1])).email
-
+      let decodedJwt = decodeJwt(authToken)
+      email = decodedJwt.email
     }
-    console.log(email);
-    
+        
     return (
         <>
         <ul>
           <li>
-            <Link to="/register"> Register</Link>
-            <Link to="/login"> Login</Link>
-            <Link to="/users"> Users</Link>
-            <Link to="/logout"> Logout</Link>
+            {
+              !authToken ? <>
+                <Link to="/register"> Register</Link>
+                <Link to="/login"> Login</Link>
+              </> :
+              <>
+                <Link to="/users"> Users</Link>
+                <Link to="/logout"> Logout</Link>              
+              </>  
+            }
           </li>  
         </ul>
         {email && <p>Hello {email }<b></b></p> }
